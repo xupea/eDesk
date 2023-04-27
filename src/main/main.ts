e6576ad1b3ec20b4c00b1aa813b24a3f60238aaa
unknown <sysu.peter.hsu@gmail.com>
2023-04-27 11:16:06 +0800

    feat: close app on mac

--- src/main/main.ts
+++ src/main/main.ts
@@ -61,11 +61,7 @@ if (!gotTheLock) {
  */
 
 app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app
